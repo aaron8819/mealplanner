be13c1532fd48c24250613bd21898b801a6e9395
aaron8819 <aabloch@microsoft.com>
2025-05-19 15:26:23 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
+  root: '.', // ensure root is correct
   plugins: [react()],
-  publicDir: 'public',
   build: {
+    rollupOptions: {
+      input: path.resolve(__dirname, 'public/index.html'),
+    },
     outDir: 'dist'
-  }
+  },
+  publicDir: 'public',
 });
